0c610d1ac31d3ade3940b845f0406141488bd7cc
Nick Sacco <nicksacco10@gmail.com>
2023-05-19 13:56:30 -0700

    feat: model test and method for total_cost

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Recipe, type: :model do
+  before(:each) do
+    test_data
+  end
    
   describe "validations" do
     it {should validate_presence_of :name}
@@ -11,5 +14,15 @@ RSpec.describe Recipe, type: :model do
   describe "relationships" do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
+  end
+
+  describe "instance methods" do
+    describe "#total_cost" do
+      it "can determine the total cost of a recipe" do
+        expect(@burger.total_cost).to eq(12)
+        expect(@gc.total_cost).to eq(5)
+        expect(@struggle.total_cost).to eq(3)
+      end
+    end
   end
 end
