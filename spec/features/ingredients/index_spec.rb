84e5e38f6b093f6c1c0dff05925ab83bea721c70
Nick Sacco <nicksacco10@gmail.com>
2023-05-19 13:00:48 -0700

    us1 test setup

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe "Ingredient Index page '/ingredient'", type: :feature do
   before(:each) do
     test_data
   end
+
+  describe "When I visit '/ingredients'" do
+    it "I see all ingredient names and costs for each" do
+      visit '/ingredients'
+
+      within "#ingredient_#{@beef.id}" do
+        expect(page).to have_content("Name: #{@beef.name}")
+        expect(page).to have_content("Cost: #{@beef.cost}")
+        expect(page).to_not have_content("Name: #{@cheese.name}")
+      end
+      within "#ingredient_#{@cheese.id}" do
+        expect(page).to have_content("Name: #{@cheese.name}")
+        expect(page).to have_content("Cost: #{@cheese.cost}")
+        expect(page).to_not have_content("Name: #{@beef.name}")
+      end
+      within "#ingredient_#{@bun.id}" do
+        expect(page).to have_content("Name: #{@bun.name}")
+        expect(page).to have_content("Cost: #{@bun.cost}")
+        expect(page).to_not have_content("Name: #{@tomato.name}")
+      end
+    end
+  end
 end
