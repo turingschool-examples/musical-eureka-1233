3a126fe892235ee0866f377d8186a1781c935793
Nick Sacco <nicksacco10@gmail.com>
2023-05-19 13:36:43 -0700

    test: us2 tests

---
+++ spec/features/recipes/show_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "Recipe Show Page", type: :feature do
+  before(:each) do
+    test_data
+  end
+
+  describe "When I visit a recipe show page /recipes/:id" do
+    it "shows the recipe name, complexity, and genre" do
+      visit "/recipes/#{@burger.id}"
+
+      expect(page).to have_content("Recipe: #{@burger.name}")
+      expect(page).to have_content("Complexity: #{@burger.complexity}")
+      expect(page).to have_content("Genre: #{@burger.genre}")
+      expect(page).to_not have_content("#{@gc.name}")
+      expect(page).to_not have_content("#{@struggle.name}")
+
+      visit "/recipes/#{@gc.id}"
+
+      expect(page).to have_content("Recipe: #{@gc.name}")
+      expect(page).to have_content("Complexity: #{@gc.complexity}")
+      expect(page).to have_content("Genre: #{@gc.genre}")
+      expect(page).to_not have_content("#{@burger.name}")
+      expect(page).to_not have_content("#{@struggle.name}")
+
+      visit "/recipes/#{@struggle.id}"
+
+      expect(page).to have_content("Recipe: #{@struggle.name}")
+      expect(page).to have_content("Complexity: #{@struggle.complexity}")
+      expect(page).to have_content("Genre: #{@struggle.genre}")
+      expect(page).to_not have_content("#{@burger.name}")
+      expect(page).to_not have_content("#{@gc.name}")
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,21 @@ def test_data
   @cheese = Ingredient.create!(name: "Yellow Kind", cost: 2)
   @tomato = Ingredient.create!(name: "Big Tomato", cost: 5)
   @bun = Ingredient.create!(name: "Burger Bun", cost: 1)
+  @bread = Ingredient.create!(name: "Bread", cost: 3)
+
+  @burger = Recipe.create!(name: "Burger", complexity: 4, genre: "American")
+  @gc = Recipe.create!(name: "Grilled Cheese", complexity: 2, genre: "American")
+  @struggle = Recipe.create!(name: "Struggle", complexity: 1, genre: "All")
+
+  RecipeIngredient.create!(recipe: @burger, ingredient: @beef)
+  RecipeIngredient.create!(recipe: @burger, ingredient: @cheese)
+  RecipeIngredient.create!(recipe: @burger, ingredient: @tomato)
+  RecipeIngredient.create!(recipe: @burger, ingredient: @bun)
+
+  RecipeIngredient.create!(recipe: @gc, ingredient: @bread)
+  RecipeIngredient.create!(recipe: @gc, ingredient: @cheese)
+
+  RecipeIngredient.create!(recipe: @struggle, ingredient: @bread)
 end
 
 
