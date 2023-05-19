6d51258aa2e04fc2aeff07ef0f5a4e1f523faf1b
Jesse Sorman <jessesorman@gmail.com>
2023-05-19 17:10:04 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+    recipe_1 = Recipe.create!(name: "Cheeseburgers", complexity: 1, genre: "BBQ")
+    ingredient_1 = recipe_1.ingredients.create!(name: "Cheese", cost: 4)
+    ingredient_2 = recipe_1.ingredients.create!(name: "Burger", cost: 10)
+    ingredient_3 = recipe_1.ingredients.create!(name: "Buns", cost: 3)
+    recipe_2 = Recipe.create!(name: "Ravioli", complexity: 3, genre: "Italian")
+    ingredient_4 = recipe_2.ingredients.create!(name: "Flour", cost: 5)
+    ingredient_5 = recipe_2.ingredients.create!(name: "Water", cost: 0)
+    ingredient_6 = recipe_2.ingredients.create!(name: "Cheese", cost: 6)
