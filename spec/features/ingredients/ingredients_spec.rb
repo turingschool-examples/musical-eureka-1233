5efeab1c4ec1000f90fa7ce669f4e08b47126876
Lauren Golden <llgolden@gmail.com>
2023-05-21 10:08:53 -0600

    User story 1 setup

---
+++ spec/features/ingredients/ingredients_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "the ingredients index" do
+# User Story 1 - Ingredients Index
+  it "displays ingredients" do 
+    ingredient1 = Ingredient.create!(name: "Pasta", cost: 2)
+    ingredient1 = Ingredient.create!(name: "Garlic", cost: 3)
+
+    visit "/ingredients"
+
+    expect(page).to have_content(ingredient1.name)
+    expect(page).to have_content(ingredient1.cost)
+    expect(page).to have_content(ingredient2.name)
+    expect(page).to have_content(ingredient2.cost)
+  end
+end
