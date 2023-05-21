d0a0962b29f8cc8be4f19f7246d7ddbd73ba8f21
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-05-21 16:20:29 -0600

    feat: total_cost method

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,7 +1,19 @@
 require "rails_helper"
 
 RSpec.describe Recipe, type: :model do
-   
+  before :each do 
+    @spaghetti = Recipe.create!(name: 'Spaghetti', complexity: 2, genre: 'Feels good')
+    @ground_beef = Ingredient.create!(name: 'ground beef', cost: 15)
+    @garlic = Ingredient.create!(name: 'garlic', cost: 3)
+    @onion = Ingredient.create!(name: 'onion', cost: 4)
+    @salt = Ingredient.create!(name: 'salt', cost: 2)
+    @tomatoes = Ingredient.create!(name: 'tomatoes', cost: 10)
+    RecipeIngredient.create!(ingredient: @tomatoes, recipe: @spaghetti)
+    RecipeIngredient.create!(ingredient: @garlic, recipe: @spaghetti)
+    RecipeIngredient.create!(ingredient: @onion, recipe: @spaghetti)
+    RecipeIngredient.create!(ingredient: @salt, recipe: @spaghetti)
+  end 
+
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :complexity}
@@ -11,5 +23,11 @@ RSpec.describe Recipe, type: :model do
   describe "relationships" do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
+  end
+
+  describe "methods" do 
+    it 'total_cost' do 
+      expect(@spaghetti.total_cost).to eq(19)
+    end
   end
 end
