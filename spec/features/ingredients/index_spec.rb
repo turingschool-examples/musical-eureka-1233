2c592b7964e1b645f5d7846216328cc49801ea44
Ashley Plasket <ashley.plasket@gmail.com>
2023-05-21 13:01:35 -0400

    user story 1 - add index page, test, view, etc

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe "/ingredients, ingredients index page", type: :feature do
+  before(:each) do
+    @ingredient1 = Ingredient.create!(name: "Ground Beef", cost: 2)
+    @ingredient2 = Ingredient.create!(name: "Salt", cost: 4)
+  end
   
+  describe "as a visitor to /ingredients page" do
+    it "should see a list of all ingredients and their attributes" do
+      visit "/ingredients"
+      
+      expect(page).to have_content("All Ingredients")
+      expect(page).to have_content(@ingredient1.name)
+      expect(page).to have_content(@ingredient1.cost)
+      expect(page).to have_content(@ingredient2.name)
+      expect(page).to have_content(@ingredient2.cost)
+    end
+  end
 end
