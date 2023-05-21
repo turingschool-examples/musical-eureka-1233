589d3a62a8c5d7db227d37211724e034f663ab90
Lauren Golden <llgolden@gmail.com>
2023-05-21 10:42:59 -0600

    Complete US1

--- spec/features/ingredients/ingredients_spec.rb
+++ spec/features/ingredients/ingredients_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.describe "the ingredients index" do
 # User Story 1 - Ingredients Index
-  xit "displays ingredients" do 
+  it "displays ingredients" do 
     ingredient1 = Ingredient.create!(name: "Pasta", cost: 2)
-    ingredient1 = Ingredient.create!(name: "Garlic", cost: 3)
+    ingredient2 = Ingredient.create!(name: "Garlic", cost: 3)
 
     visit "/ingredients"
-
+save_and_open_page
     expect(page).to have_content(ingredient1.name)
     expect(page).to have_content(ingredient1.cost)
     expect(page).to have_content(ingredient2.name)
