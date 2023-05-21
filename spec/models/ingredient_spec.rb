024796136604f14440d0590f196c325239d08c1a
Amy Spears <amspears007@gmail.com>
2023-05-21 13:08:48 -0600

    Add test for sort_a_z class method

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Ingredient, type: :model do
+   let!(:chicken) {Ingredient.create!(name: "Chicken", cost: 10)}
+   let!(:ravioli) {Ingredient.create!(name: "Ravioli", cost: 2)}
+   let!(:cheese) {Ingredient.create!(name: "Parmesan", cost:5)}
+   let!(:sauce) {Ingredient.create!(name: "Marinara", cost:3)}
+   let!(:bread_crumbs) {Ingredient.create!(name: "Bread Crumbs", cost:1)}
 
    describe "validations" do
       it { should validate_presence_of :name }
@@ -10,6 +15,12 @@ RSpec.describe Ingredient, type: :model do
    describe "relationships" do
       it { should have_many :recipe_ingredients }
       it { should have_many(:recipes).through(:recipe_ingredients) }
+   end
+
+   describe "Sorts Ingredients Alphabetically" do
+      it "# sorts_a_z" do
+         expect(Ingredient.sorts_a_z).to eq [bread_crumbs, chicken, sauce, cheese, ravioli]
+      end
    end
 
 end
