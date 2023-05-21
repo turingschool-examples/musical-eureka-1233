221aa89198493e3c0e700fa51ec0674036b69966
Sarah Garlock <garlock.sarah10@gmail.com>
2023-05-21 10:42:18 -0600

    add spec for ingredients index

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,30 @@
+# As a visitor,
+# When I visit '/ingredients'
+# I see a list of all the ingredients including their name and cost
+# (e.g. "Ground Beef: 2"
+#      "Salt: 4")
+require "rails_helper"
+
+RSpec.describe "/ingredients", type: :feature do
+  describe "As a visitor, when I visit the ingredients index page" do
+    before(:each) do
+      @ingredient_1 = Ingredient.create!(name: "Ground Beef", cost: 2)
+      @ingredient_2 = Ingredient.create!(name: "Salt", cost: 4)
+      @ingredient_3 = Ingredient.create!(name: "Butter", cost: 5)
+
+      visit "/ingredients"
+    end
+
+    it "will list of all the ingredients including their name and cost" do
+      expect(page).to have_content(@ingredient_1.name)
+      expect(page).to have_content(@ingredient_1.cost)
+      
+      expect(page).to have_content(@ingredient_2.name)
+      expect(page).to have_content(@ingredient_2.cost)
+
+      expect(page).to have_content(@ingredient_3.name)
+      expect(page).to have_content(@ingredient_3.cost)
+save_and_open_page
+    end
+  end
+end
