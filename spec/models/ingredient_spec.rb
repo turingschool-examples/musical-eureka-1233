5940aa2fbf358be2844ba08aa8bd38b6bd018752
K.D. Hubbard <hubbard.kd@gmail.com>
2023-05-22 11:58:01 -0600

    add test for order by name methdo

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Ingredient, type: :model do
-
+   before(:each) do 
+      @ingredient_1 = Ingredient.create!(name: "TVP", cost: 3)
+      @ingredient_2 = Ingredient.create!(name: "Celery", cost: 1)
+      @ingredient_3 = Ingredient.create!(name: "Carrots", cost: 2)
+      @ingredient_4 = Ingredient.create!(name: "Parsnips", cost: 1)
+      @ingredient_5 = Ingredient.create!(name: "Vital Wheat Gluten", cost: 3)
+      @ingredient_6 = Ingredient.create!(name: "Onion", cost: 4)
+      @ingredient_7 = Ingredient.create!(name: "Garlic", cost: 2)
+   end
    describe "validations" do
       it { should validate_presence_of :name }
       it { should validate_presence_of :cost }
@@ -12,4 +20,9 @@ RSpec.describe Ingredient, type: :model do
       it { should have_many(:recipes).through(:recipe_ingredients) }
    end
 
+   describe "class methods" do 
+      it "order_by_name" do 
+         expect(Ingredient.order_by_name).to eq([@ingredient_3, @ingredient_2, @ingredient_7, @ingredient_6, @ingredient_4, @ingredient_1, @ingredient_5])
+      end
+   end
 end
