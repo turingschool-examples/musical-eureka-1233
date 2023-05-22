8ed00c6ffba770ac831ec589cbd36e8411ebd2d0
Alex Kiselich <alexander.kiselich@gmail.com>
2023-05-22 14:54:33 -0600

    User Story 1

---
+++ spec/features/ingredients/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "/ingrediants" do
+  before :each do
+    @milk = Ingredient.create!(name: "Milk", cost: 2)
+    @flour = Ingredient.create!(name: "Flour", cost: 1)
+
+    @cake = Recipe.create!(name: "Cookies", complexity: 3, genre: "Desert")
+    @soup = Recipe.create!(name: "Pea Soup", complexity: 4, genre: "Dinner")
+  end
+  describe "As a visitor, when I visit the ingrediants index page" do
+    it 'Displays a list of all the ingrediants including their name and cost' do
+      visit "/ingredients"
+      expect(page).to have_content(@milk.name)
+      expect(page).to have_content(@milk.cost)
+      expect(page).to have_content(@flour.name)
+      expect(page).to have_content(@flour.cost)
+    end
+    
+  end
+end
