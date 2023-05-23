755a8f901beb72944002048fdc106ff806dadbf6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-05-23 16:12:51 -0600

    Test: add tests to make ingredients list in alphabetical order

--- spec/features/ingredients/index_spec.rb
+++ spec/features/ingredients/index_spec.rb
@@ -9,9 +9,10 @@ require "rails_helper"
 #      "Salt: 4")
 #US 1
 RSpec.describe "the Ingredient Index page" do
-  let!(:ingredient_1) { Ingredient.create!(name: "Ground Beef", cost: "2")}
-  let!(:ingredient_2) { Ingredient.create!(name: "Salt", cost: "4")}
-
+  let!(:ingredient_1) { Ingredient.create!(name: "Yams", cost: "77")}
+  let!(:ingredient_2) { Ingredient.create!(name: "Ground Beef", cost: "2")}
+  let!(:ingredient_3) { Ingredient.create!(name: "Salt", cost: "4")}
+  
   describe "When a visitor visits the index page" do
     it "should display all the ingredients, including their name and cost" do
       visit "/ingredients"
@@ -22,6 +23,17 @@ RSpec.describe "the Ingredient Index page" do
       expect(page).to have_content("4")
       expect(page).to_not have_content("Chicken")
       expect(page).to_not have_content("3")
+    end
+  end
+
+  describe "Extention 1" do
+    it "As a visitor when I visit the ingredients index page, it displays the list of ingredients alphabetically" do
+      visit "/ingredients"
+      save_and_open_page
+
+      expect(page).to have_content(ingredient_1.name)
+      expect(page).to have_content(ingredient_2.name)
+      expect(page).to have_content(ingredient_3.name)
     end
   end
 end
