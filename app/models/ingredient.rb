7e215140bc2c23781608148953ee4859336a38f4
Bradley Milton <bmilton2412@gmail.com>
2023-05-22 20:43:22 -0700

    adds associations to ingredient, recipe ingredient, and recipe

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,12 @@
 class Ingredient < ApplicationRecord
    validates_presence_of :name, :cost
+   has_many :recipe_ingredients
+   has_many :recipes, through: :recipe_ingredients
 end
+
+# class Pet < ApplicationRecord
+#    validates :name, presence: true
+#    validates :age, presence: true, numericality: true
+#    belongs_to :shelter
+#    has_many :pet_applications
+#    has_many :applications, through: :pet_applications
